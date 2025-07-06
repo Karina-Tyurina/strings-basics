44bcc07ed086783c933c744bcab3ebaafbe71cb1
origin <13devo4ka@mail.ru>
2025-07-06 20:05:02 +0300

    строки д.з

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = "Фамилия Имя Отчество";
+        fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
 
+        System.out.println("Задание 2");
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullName1);
+
+        System.out.println("Задание 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName2);
     }
 }
